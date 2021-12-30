fd9020e54a75d320d350ab7bdb34b187c6c50b56
Francis kinyuru <49100234+franciskinyuru@users.noreply.github.com>
2021-12-30 10:48:32 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,7 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTPRhW7gFFFwJbWXUuyJGMO2ntcOft4uYgT/RmtQdoR0W7ttN/hsC8m/d1bcY6kdEpsbrhIHtHdIMO7uuJ7eDy4AK5Fb+lN6Z8j5OaxKZMdUFDD34o4TTF1+oLIwlR/ZLBIlDarOtPePGLZ3TP/AAeOv9F
-zpipa/HqeCvwgphC5yu9IjMfXNYktFjdCJ9MC+q+H+Eglkvz0/NfLCWbE0sPEVrcPbYc81NAK6dX7atdyY76kebFTrbQiqBjmGR5I9DiGOrK2ZsTY7d31oE2Z7v/5QyNgMJP1NBqi0QI0A30ynLzDcHl6Y6ZWim5ssXIewYhbMAjJ17OvmBXJvXDK54j',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTPRhW7gFFFwJbWXUuyJGMO2ntcOft4uYgT/RmtQdoR0W7ttN/hsC8m/d1bcY6kdEpsbrhIHtHdIMO7uuJ7eDy4AK5Fb+lN6Z8j5OaxKZMdUFDD34o4TTF1+oLIwlR/ZLBIlDarOtPePGLZ3TP/AAeOv9Fzpipa/HqeCvwgphC5yu9IjMfXNYktFjdCJ9MC+q+H+Eglkvz0/NfLCWbE0sPEVrcPbYc81NAK6dX7atdyY76kebFTrbQiqBjmGR5I9DiGOrK2ZsTY7d31oE2Z7v/5QyNgMJP1NBqi0QI0A30ynLzDcHl6Y6ZWim5ssXIewYhbMAjJ17OvmBXJvXDK54j',
   }  
 }
