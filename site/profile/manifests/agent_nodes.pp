52bf870c6ad100c6329c7e2cab1b8d19ed517d49
Francis kinyuru <49100234+franciskinyuru@users.noreply.github.com>
2021-12-30 12:25:37 +0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,6 +4,8 @@ dockeragent::node{'web.puppet.vm':
 }
 dockeragent::node{'db.puppet.vm':
 }
+dockeragent::node{'minetest.puppet.vm':
+}
 host {'web.puppet.vm':
 ensure => present,
 ip     => '172.18.0.2'
