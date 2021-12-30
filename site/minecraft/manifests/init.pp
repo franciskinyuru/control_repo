671eee2caccd9b0f9a52a8667af992861693d61f
Francis kinyuru <49100234+franciskinyuru@users.noreply.github.com>
2021-12-30 15:09:13 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,5 +17,8 @@ file{'/etc/systemd/system/minecraft.service':
 ensure => file,
 source => 'puppet:///modules/minecraft/minecraft.service',
 }
-
+service {'minecraft':
+ensure => running,
+enable => true,
+}
 }
